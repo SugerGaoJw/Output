80b195814ad6d711ef1c0067f677f0c9dc6a9835
Tiger83Zhang <zhangsx@fj.10086.cn>
2015-10-14 17:14:28 +0800

    test

---
+++ baseProject/requirementDoc.h
@@ -0,0 +1,39 @@
+//
+//  requirementDoc.h
+//  baseProject
+//
+//  Created by ZhangSx on 15/10/14.
+//  Copyright (c) 2015年 Li. All rights reserved.
+//
+
+#ifndef baseProject_requirementDoc_h
+#define baseProject_requirementDoc_h
+/*
+ 03_1 a  3.5
+ 03_2 m  1.5
+ 03_5 ?
+ 03_7 m  1.5
+ 04_1 ?
+ 04_2 ?
+ 04_3 m  0.5
+ 04_4 a  4
+ 05   m  1
+ 06 1 m  0.5
+ 07 + m 改文字 0.5
+ 07_2 m 1
+ 08_3 m 1
+ 08_4 m 1
+ 08_7 m 0.5
+ 08_9 m 1.5
+ 09_1 可能要修改 0.5
+ 
+ 可拖动控件 3
+ 
+ 27，95————
+ 
+ 银联支付—— 5   55
+ 
+ ps: a为新增  m为修改  ?为与旧版不同便暂不做变动
+ */
+
+#endif
